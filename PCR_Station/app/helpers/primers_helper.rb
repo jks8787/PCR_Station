5a0fa469b1df4b13b1652738cf5bdccd6cc72cac
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-04 10:04:29 -0500

    now can calculate melting temp of primers

--- PCR_Station/app/helpers/primers_helper.rb
+++ PCR_Station/app/helpers/primers_helper.rb
@@ -1,2 +1,23 @@
 module PrimersHelper
+	# The simplest formula is as follows (1):
+
+	# Tm = 4°C  x  (number of G’s and C’s in the primer) + 2°C  x  (number of A’s and T’s in the primer)
+
+	# This formula is valid for oligos <14 bases and assumes that the reaction is carried out
+	#in the presence of 50mM monovalent cations. For longer oligos, the formula below is used:
+
+	# Tm =  64.9°C + 41°C x (number of G’s and C’s in the primer – 16.4)/N
+
+	# Where N is the length of the primer.
+
+	def melting_temp(primer_seq)
+	primer_seq = Bio::Sequence::NA.new(primer_seq)
+	counts = {'a'=>primer_seq.count('a'),'c'=>primer_seq.count('c'),'g'=>primer_seq.count('g'),'t'=>primer_seq.count('t')}
+		if primer_seq.length <= 14
+			(4 * (counts['c'] + counts['g'] )) + (2 * (counts['a'] + counts['t']))
+		else primer_seq.length > 14
+			64.9 + 41 * ((counts['c'] + counts['g'] ) - 16.4)/ primer_seq.length
+		end
+	end
+
 end
