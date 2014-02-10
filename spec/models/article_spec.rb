817cd3b288571a312d4c7ccc6f9f5070712247c6
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-10 12:30:29 -0500

    testing validation in article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Article do
 
+	describe "validations" do
+		it { should validate_uniqueness_of(:uid) }
+	end
+
   describe "associations" do
   	it { should have_and_belong_to_many :users }
 	end
