3eaff8c67379b07f0bd462ea3b753e873a533c08
Janice Smith <janice.k.n.smith@gmail.com>
2014-04-10 10:58:22 -0400

    cleaning up the front page - new order for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     Article.fetch_from_api()
-    @articles = Article.all
+    @articles = Article.order(created_at: :desc).limit(20)
   end
 
 end
